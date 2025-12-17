e794ac39683d192919f20da9696399b491d8784b
aleksandraaa505 <159193578+aleksandraaa505@users.noreply.github.com>
2025-12-16 22:55:06 -0500

    Delete infrastructure/scripts/mongo-init.js

--- infrastructure/scripts/mongo-init.js
+++
@@ -1,9 +0,0 @@
-// Simple Mongo init script for Closet-X (placeholder)
-print("Running mongo-init.js...");
-
-db = db.getSiblingDB("closetx");
-
-if (!db.getCollectionNames().includes("users")) {
-  db.createCollection("users");
-}
-
